5c579993e78a7ea59a79e105b40813164c4edce0
Russell Roberts <russrob23@gmail.com>
2016-09-12 11:01:35 -0700

    Adde pages to the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'pages/home'
 
+root 'pages#home'
+
+get 'about' => 'pages#about'
+
+
+get 'pricing' => 'pages#pricing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
